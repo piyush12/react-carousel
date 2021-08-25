10ca2b705fe916bee6b34609ab43c3a2a2608d8a
piyush12 <pcpiyus480@gmail.com>
2021-08-25 22:57:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { CarouselItem, Carousel } from "./Carousel";
+
+export default function App() {
+  return (
+    <Carousel>
+      <CarouselItem>1</CarouselItem>
+      <CarouselItem>2</CarouselItem>
+      <CarouselItem>3</CarouselItem>
+    </Carousel>
+  );
+}
